8e26043809d749b2603ecce69de5f4253796032a
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-07-04 23:01:38 +0930

    feat(serialisation): created Kotlin extension functions for Gson

---
+++ src/main/kotlin/serialisation/GsonExt.kt
@@ -0,0 +1,9 @@
+package serialisation
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+/**
+ * Converts the supplied JSON structure to an instance of [T].
+ */
+inline fun <reified T> Gson.fromJson(json: String) = this.fromJson<T>(json, object : TypeToken<T>() {}.type)
