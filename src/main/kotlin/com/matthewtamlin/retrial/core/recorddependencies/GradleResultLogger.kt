61af809d86af7797662a1978a977c0dc239b001a
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-07-09 23:09:39 +0930

    feat(recorddependencies): created gradle result logger

---
+++ src/main/kotlin/com/matthewtamlin/retrial/core/recorddependencies/GradleResultLogger.kt
@@ -0,0 +1,10 @@
+package com.matthewtamlin.retrial.core.recorddependencies
+
+import io.reactivex.Completable
+import org.gradle.api.logging.Logger
+
+class GradleResultLogger(private val logger: Logger) : ResultLogger {
+  override fun logSuccess() = Completable.fromRunnable { logger.info("Dependency checksums recorded successful.")}
+
+  override fun logFailure() = Completable.fromRunnable{logger.error("Failed to record dependency checksums.")}
+}
