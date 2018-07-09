c2c1bbd8dfa24b4cf23856c327506187f3befb6d
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-07-09 23:12:34 +0930

    feat(verifydependencies): created dagger modules

---
+++ src/main/kotlin/com/matthewtamlin/retrial/core/verifydependencies/TaskRunnerModule.kt
@@ -0,0 +1,22 @@
+package com.matthewtamlin.retrial.core.verifydependencies
+
+import com.matthewtamlin.retrial.checksum.Sha512ChecksumGenerator
+import com.matthewtamlin.retrial.core.Crasher
+import dagger.Module
+import dagger.Provides
+import com.matthewtamlin.retrial.dependencies.live.ProjectBasedLiveDependenciesRepository
+import com.matthewtamlin.retrial.dependencies.saved.SavedDependenciesRepository
+import javax.inject.Singleton
+
+@Module
+class TaskRunnerModule {
+  @Provides
+  @Singleton
+  fun provideTaskRunner(
+      savedDependenciesRepository: SavedDependenciesRepository,
+      liveDependenciesRepository: ProjectBasedLiveDependenciesRepository,
+      checksumGenerator: Sha512ChecksumGenerator,
+      resultLogger: ResultLogger,
+      crasher: Crasher
+  ) = TaskRunner(savedDependenciesRepository, liveDependenciesRepository, checksumGenerator, resultLogger, crasher)
+}
