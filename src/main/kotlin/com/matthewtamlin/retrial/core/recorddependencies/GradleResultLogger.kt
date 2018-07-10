b55436c73cf088863c0d0bdef66ca8ae05ec0cb6
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-07-11 00:12:15 +0930

    style(recorddependencies): changed spacing

--- src/main/kotlin/com/matthewtamlin/retrial/core/recorddependencies/GradleResultLogger.kt
+++ src/main/kotlin/com/matthewtamlin/retrial/core/recorddependencies/GradleResultLogger.kt
@@ -4,7 +4,7 @@ import io.reactivex.Completable
 import org.gradle.api.logging.Logger
 
 class GradleResultLogger(private val logger: Logger) : ResultLogger {
-  override fun logSuccess() = Completable.fromRunnable { logger.info("Dependency checksums recorded successful.")}
+  override fun logSuccess() = Completable.fromRunnable { logger.info("Dependency checksums recorded successful.") }
 
-  override fun logFailure() = Completable.fromRunnable{logger.error("Failed to record dependency checksums.")}
+  override fun logFailure() = Completable.fromRunnable { logger.error("Failed to record dependency checksums.") }
 }
