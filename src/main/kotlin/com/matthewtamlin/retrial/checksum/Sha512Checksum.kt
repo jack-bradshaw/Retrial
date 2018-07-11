e1983ba858f12f095cbd1455459095d757f751f9
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-07-11 21:23:18 +0930

    docs(checksum): added kDoc

--- src/main/kotlin/com/matthewtamlin/retrial/checksum/Sha512Checksum.kt
+++ src/main/kotlin/com/matthewtamlin/retrial/checksum/Sha512Checksum.kt
@@ -1,3 +1,8 @@
 package com.matthewtamlin.retrial.checksum
 
+/**
+ * A SHA2-512 hash of some data.
+ *
+ * @property value the literal value of the hash
+ */
 data class Sha512Checksum(val value: String)
