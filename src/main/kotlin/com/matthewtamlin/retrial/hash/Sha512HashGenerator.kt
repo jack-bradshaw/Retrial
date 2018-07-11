66b889b982dc9170f0775c4a873ee07d8d2dd58c
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-07-11 21:59:43 +0930

    docs(hash): changed kDoc

--- src/main/kotlin/com/matthewtamlin/retrial/hash/Sha512HashGenerator.kt
+++ src/main/kotlin/com/matthewtamlin/retrial/hash/Sha512HashGenerator.kt
@@ -6,10 +6,15 @@ import java.nio.file.Files
 import java.security.MessageDigest
 import javax.inject.Provider
 
-
+/**
+ * Generates SHA2-512 hashes from files.
+ */
 class Sha512HashGenerator {
   private val sha512DigestProvider: Provider<MessageDigest> = Provider { MessageDigest.getInstance("SHA-512") }
 
+  /**
+   * Generates a SHA2-512 hash of the contents of a file.
+   */
   fun generateChecksum(file: File) = Single.fromCallable {
     if (file.length() == 0L) {
       throw RuntimeException("Cannot calculate hash for empty file.")
