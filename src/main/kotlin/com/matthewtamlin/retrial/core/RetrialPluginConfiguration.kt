f89ea184128b57b2080b62082d6aaa0921ec787d
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-07-11 18:05:13 +0930

    docs(core): added Javadoc

--- src/main/kotlin/com/matthewtamlin/retrial/core/RetrialPluginConfiguration.kt
+++ src/main/kotlin/com/matthewtamlin/retrial/core/RetrialPluginConfiguration.kt
@@ -2,6 +2,12 @@ package com.matthewtamlin.retrial.core
 
 import java.io.File
 
+/**
+ * Provides configuration control for the Retrial plugin.
+ */
 open class RetrialPluginConfiguration {
+  /**
+   * Overrides the file to use to store dependency checksums.
+   */
   var checksumFile: File? = null
 }
