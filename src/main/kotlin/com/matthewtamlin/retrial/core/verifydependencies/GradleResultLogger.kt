70bb5b5b9442fa6cbe37c17733ed1a46eb18e941
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-07-11 22:10:47 +0930

    feat(verifydependencies): formatted dependencies when printing to log

--- src/main/kotlin/com/matthewtamlin/retrial/core/verifydependencies/GradleResultLogger.kt
+++ src/main/kotlin/com/matthewtamlin/retrial/core/verifydependencies/GradleResultLogger.kt
@@ -1,12 +1,13 @@
 package com.matthewtamlin.retrial.core.verifydependencies
 
+import com.matthewtamlin.retrial.dependencies.DependencyKey
 import io.reactivex.Completable
 import org.gradle.api.logging.Logger
 
 /**
  * A [ResultLogger] that formats logs and delegates to a [Logger].
  */
-class GradleResultLogger(private val logger: Logger): ResultLogger {
+class GradleResultLogger(private val logger: Logger) : ResultLogger {
   override fun logSuccess() = Completable.fromRunnable { logger.info("Dependency verification passed.") }
 
   override fun logFailureDueTo(diff: DependencyDiff) = Completable.fromRunnable {
@@ -16,22 +17,26 @@ class GradleResultLogger(private val logger: Logger): ResultLogger {
           if (!diff.additionalDependencies.isEmpty()) {
             appendln("\tAdditional dependencies:")
 
-            diff.additionalDependencies.forEach { appendln("\t\t$it") }
+            diff.additionalDependencies.forEach { appendln("\t\t${prettyFormatDependencyKey(it)}") }
           }
 
           if (!diff.missingDependencies.isEmpty()) {
             appendln("\tMissing dependencies:")
 
-            diff.missingDependencies.forEach { appendln("\t\t$it") }
+            diff.missingDependencies.forEach { appendln("\t\t${prettyFormatDependencyKey(it)}") }
           }
 
           if (!diff.changedDependencies.isEmpty()) {
             appendln("\tChanged dependencies:")
 
-            diff.changedDependencies.forEach { appendln("\t\t$it") }
+            diff.changedDependencies.forEach { appendln("\t\t${prettyFormatDependencyKey(it)}") }
           }
         }
         .toString()
         .also { logger.error(it) }
+  }
+
+  private fun prettyFormatDependencyKey(key: DependencyKey):String {
+    return "group: ${key.group}, name: ${key.name}, version: ${key.version}"
   }
 }
